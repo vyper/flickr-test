290067819acd03b12a24ec4d8a618269ff4f67a4
Leonardo Saraiva <vyper@maneh.org>
2015-02-01 02:41:34 +0000

    Added integration spec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/spec'
+require 'minitest/rails'
+require 'minitest/rails/capybara'
+
+require "minitest/reporters"
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
